554be60b067d1025567e349202bace0f8edc1ac7
dominiquem <dominique.meaux@gmail.com>
2018-06-22 14:39:38 +0200

    onKeyPress

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function onKeyPress(){
+// Déclaration et récupération des variable
+var lastname = document.getElementById('lastname').value;
+// Boite de dialogue déclanché par un appui sur touche
+  alert(lastname);
+}
